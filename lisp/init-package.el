ff75b4f56f25a4cf432d675c89c3a9504bf064fa
Jiajie Chen <c@jia.je>
2023-07-30 22:45:41 +0800

    Cleanup code

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -6,83 +6,18 @@
 
 ;;; Code:
 
-;; Pending: company-coq dict ghci go lua math shell web
-
-;; From https://glyph.twistedmatrix.com/2015/11/editor-malware.html
-;; (setq-default tls-checktrust t)
-;; (let ((trustfile "/usr/local/etc/openssl/cert.pem"))
-;;   (setq-default tls-program
-;;                 (list
-;;                  (format "gnutls-cli%s --x509cafile %s -p %%p %%h"
-;;                          (if (eq window-system 'w32) ".exe" "") trustfile)))
-;;   (setq-default gnutls-verify-error t
-;;                 gnutls-trustfiles (list trustfile)))
-
+;; Setup TUNA mirrors
 (require 'package)
-;; (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-;; 			 ("org" . "http://orgmode.org/elpa/") ; Hope to use https
-;; 			 ("melpa" . "https://melpa.org/packages/")
-;; 			 ))
-;; (setq package-archives '(("ec-gnu" . "https://git.oschina.net/EmacsChina/elpa/raw/master/gnu/")
-;; 			 ("ec-org" . "https://git.oschina.net/EmacsChina/elpa/raw/master/org/")
-;; 			 ("ec-melpa" . "https://git.oschina.net/EmacsChina/elpa/raw/master/melpa/")))
-;; (package-initialize)
 (setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
                          ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-
-;; (setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-;;                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-;;                          ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-;;                          ("sunrise-commander" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/sunrise-commander/")))
 (setq load-prefer-newer t)
 (package-initialize)
-;; Oh no I want to use HTTPS!
-;; (setq package-archives '(("ec-gnu" . "http://elpa.emacs-china.org/gnu/")
-;; 			 ("ec-org" . "http://elpa.emacs-china.org/org/")
-;; 			 ("ec-melpa" . "http://elpa.emacs-china.org/melpa/")))
-;; (package-initialize)
 
-;; (when (and (fboundp 'daemonp)
-;;            (daemonp))
-;;   (package-refresh-contents))
-
-;; (unless (require 'quelpa nil t)
-;;   (with-temp-buffer
-;;     (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
-;;     (eval-buffer)))
-;; (setq quelpa-checkout-melpa-p nil)
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-;; temporary fix
-;; (quelpa '(use-package :fetcher github :repo "jwiegley/use-package"))
+;; Setup use-package
 (require 'use-package)
-;; (require 'diminish)
-;; (use-package diminish
-;;   :ensure t)
 (setq use-package-verbose 'debug
       use-package-minimum-reported-time 0)
-
-;; (use-package el-get
-;;   :ensure t
-;;   :config
-;;   (setq el-get-notify-type 'message))
-
-;; (use-package auto-compile
-;;   :ensure t
-;;   :config
-;;   (setq auto-compile-display-buffer nil
-;;         auto-compile-mode-line-counter t)
-;;   (auto-compile-on-load-mode)
-;;   (auto-compile-on-save-mode))
-
-;; (use-package exec-path-from-shell
-;;   :ensure t
-;;   :config
-;;   (when (memq window-system '(mac ns x))
-;;     (let ((shell-file-name "/usr/local/bin/fish"))
-;;       (exec-path-from-shell-initialize))))
 
 (provide 'init-package)
 ;;; init-package.el ends here
